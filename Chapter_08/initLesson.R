d2eca686dd4f5526164af836397001e97905d4a0
Bo Pang <bopang89@gmail.com>
2019-11-28 05:13:46 -0800

    chapter 8

--- Chapter_08/initLesson.R
+++ Chapter_08/initLesson.R
@@ -7,3 +7,12 @@ rm(list = ls())
 swirl_options(swirl_logging = TRUE)
 swirl_options(swirl_is_fun = FALSE)
 
+parks_df <- data.frame("Name"=c("Leslie","Ron","April"),"Height"=c(62,71,66),
+                       "Weight"=c(115,201,119),"Income"=c(4000,NA,2000))
+L <- list(1:10,matrix(1:6,nrow=2,ncol=3),parks_df,list(1:5,matrix(1:9,nrow=3,ncol=3)))
+
+A <- matrix(rnorm(100), ncol=5)
+
+pulse <- round(rnorm(22, 70, 2)) + rep(c(0, 5), c(10, 12))
+F1 <- sample(rep(c("A1", "A2"), c(10, 12)))
+F2 <- rep(c("B1", "B2", "B1", "B2"), c(5, 6, 5, 6))
